509d66df399e70a9064e88c4628c101a7016518e
Veronica Lopez <nicaa0695@gmail.com>
2019-07-09 17:06:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,6 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
   if library["get_meaning"].include?(japenese_emoticon)
     library["get_meaning"][japenese_emoticon]
   else
-    "Sorry, that emoticon is not known"
+    "Sorry, that emoticon was not found"
   end
 end
