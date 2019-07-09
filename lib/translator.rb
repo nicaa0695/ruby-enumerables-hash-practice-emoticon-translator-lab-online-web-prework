86aa389e0c4cc1a35a371c3f29810603b18ffe19
Veronica Lopez <nicaa0695@gmail.com>
2019-07-09 17:06:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   end
 end
 
-def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
+def get_english_meaning(file_path = './lib/emoticonsyml', japanese_emoticon)
   library = load_library(file_path)
   if library["get_meaning"].include?(japenese_emoticon)
     library["get_meaning"][japenese_emoticon]
