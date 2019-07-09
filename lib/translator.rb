63197ce2d532faa134308fd5d853e684c2297185
Veronica Lopez <nicaa0695@gmail.com>
2019-07-09 17:11:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 
 def get_japanese_emoticon(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
-  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon = emoticon_lib['get_emoticon']['emoticon']
   japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
 end
 
